801179d98b5f961b60468c793c590ff0975a4db2
Vangelis Christou <vchristou@doctoranytime.gr>
2020-05-03 08:49:15 +0300

    tony

---
+++ test.py
@@ -0,0 +1,11 @@
+print("hello py")
+numb = 5
+if numb == 10:
+    print("yes")
+else:
+    print("no 10")
+
+name = input("Enter your name:")
+for i in range(1,4):
+    print("Hello "+name + " " + str(i))
+
